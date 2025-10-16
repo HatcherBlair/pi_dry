00e954a1712f716f890313f51d5fa8798fcf2423
Hatcher Blair <hatcherbb@gmail.com>
2025-10-16 13:26:36 -0500

    transferring to pi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::i2c::I2c;
+use rppal::system::DeviceInfo;
+
+// blinking LED example
+fn led_example() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink every 500ms
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
+}
+
+// Temp and Humid Sensor Skeleton
+fn temp_sensor_callback() {}
+
 fn main() {
     println!("Hello, world!");
 }
